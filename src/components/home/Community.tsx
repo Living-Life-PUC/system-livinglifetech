0ce7c12b003fdcde464aa7ef25c1ab22b8c1ec2b
Mariana <marianaalmeidafga@gmail.com>
2024-10-29 19:44:42 -0300

    feat: comunidade finalizada

--- src/components/home/Community.tsx
+++ src/components/home/Community.tsx
@@ -1,5 +1,18 @@
+import styles from "./Community.module.scss"
+
 function Community() {
-    return (  );
+  return (
+    <section className={`${styles.community}`}>
+      <article>
+        <h2>Serviços de Qualidade</h2>
+        <h1>Personais Trainers Qualificados</h1>
+        <p>
+          Nossos treinadores são especialistas e estão prontos para ajudar você a maximizar seus
+          resultados com orientação personalizada.
+        </p>
+      </article>
+    </section>
+  );
 }
 
 export default Community;
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,5 +1,6 @@
 import About from "../components/home/About";
 import Banner from "../components/home/Banner";
+import Community from "../components/home/Community";
 import Resourses from "../components/home/Resources";
 import Trainer from "../components/home/Trainer";
 
@@ -10,6 +11,7 @@ function Home() {
       <About />
       <Resourses />
       <Trainer />
+      <Community />
     </>
   );
 }
