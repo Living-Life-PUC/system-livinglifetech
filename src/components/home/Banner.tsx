8ccb80f7f73ca053e26db62d8ffa28a553e3ab0b
Mariana <marianaalmeidafga@gmail.com>
2024-10-30 07:14:49 -0300

    feat: criando responsividade do sistema

--- src/components/home/Banner.tsx
+++ src/components/home/Banner.tsx
@@ -3,7 +3,7 @@ import styles  from "./Banner.module.scss"
 function Banner() {
   return (
     <div className={styles.banner}>
-        <h1>Encontre sua <br /> motivação e <br /><span>viva a vida!</span></h1>
+        <h1>Encontre sua motivação e <span>viva a vida!</span></h1>
     </div>
   );
 }
